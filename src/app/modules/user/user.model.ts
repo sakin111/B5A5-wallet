9de6006c087243e8d4479e53181f2224e8409230
sakin111 <maliksakin53@gmail.com>
2025-08-06 14:26:46 +0600

    .

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -15,7 +15,7 @@ const AuthSchema = new Schema<IAuth>({
 const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String },
+    password: { type: String , required : true},
     role: {
         type: String,
         enum: Object.values(Role),
