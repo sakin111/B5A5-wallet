c803e720a8db130bbb49104412f7b7bdefcb0643
sakin111 <maliksakin53@gmail.com>
2025-08-07 01:16:39 +0600

    .

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -1,13 +1,14 @@
 import { NextFunction ,Request, Response} from "express"
-import { ZodObject } from "zod"
+import { ZodObject, ZodRawShape } from "zod"
 
 
 
 
-export const validateRequest = (ZodSchema : ZodObject )=>
+export const validateRequest = (ZodSchema : ZodObject<ZodRawShape> )=>
 async(req: Request, res: Response, next: NextFunction)=>{
    try {
-     
+     console.log("Request Body:", req.body);
+
     req.body = await ZodSchema.parseAsync(req.body)
     next()
    } catch (error) {
