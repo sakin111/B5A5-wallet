b5932a82af8dce438c7dc6d22cd0576ddea42543
sakin111 <maliksakin53@gmail.com>
2025-08-07 01:27:20 +0600

    .

--- src/app/modules/transaction/transaction.validation.ts
+++ src/app/modules/transaction/transaction.validation.ts
@@ -5,30 +5,22 @@ import { z } from 'zod';
 
 
 export const SendMoneySchema = z.object({
-  body: z.object({
     toUserId:  z.string(),
     amount: z.number().positive('Amount must be positive'),
-  }),
 });
 
 export const WithdrawSchema = z.object({
-  body: z.object({
-    amount: z.number().positive('Amount must be positive'),
-  }),
+  amount: z.number().positive('Amount must be positive'),
 });
 
 
 // For agents
 export const CashInSchema = z.object({
-  body: z.object({
-    toUserId:  z.string(),
+  toUserId:  z.string(),
     amount: z.number().positive('Amount must be positive'),
-  }),
 });
 
 export const CashOutSchema = z.object({
-  body: z.object({
-    toUserId: z.string(),
+   toUserId: z.string(),
     amount: z.number().positive('Amount must be positive'),
-  }),
 });
