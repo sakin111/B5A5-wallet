0ba0f2cb088360a2ce90a81e130572b8f68858f4
sakin111 <maliksakin53@gmail.com>
2025-09-19 22:45:06 +0600

    latest commit

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -59,16 +59,16 @@ const logout = catchAsync(async (req: Request, res: Response) => {
 
   res.clearCookie("accessToken", {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax"
+    secure: true,
+    sameSite: "none"
   })
 
 
 
   res.clearCookie("refreshToken", {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax"
+    secure: true,
+    sameSite: "none"
   })
 
   sendResponse(res, {
